d93110438c4ab9e9b6e471b2f47e1f5efbb7ba5d
AleksandraLyashchenco <juicyluck007@gmail.com>
2021-09-18 11:50:13 +0300

    admin for teacher

--- vocalkiev/apps/crm/apps/teacher/forms.py
+++ vocalkiev/apps/crm/apps/teacher/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from vocalkiev.apps.crm.models import *
 
 
-class LessonTeacherAdminForm(forms.ModelForm):
+class LessonAdminForm(forms.ModelForm):
     class Meta:
         model = Lesson
         fields = '__all__'
@@ -17,12 +17,6 @@ class LessonTeacherAdminForm(forms.ModelForm):
         if not data.get('teacher'):
             data['teacher'] = data['client_subscription'].teacher
         return data
-
-
-class LessonAdminForm(forms.ModelForm):
-    class Meta:
-        model = Lesson
-        fields = '__all__'
 
 
 class PaymentAdminForm(forms.ModelForm):
@@ -52,12 +46,6 @@ class ClientCommentAdminForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
 
 
-class PaymentInlineForm(forms.ModelForm):
-    class Meta:
-        model = Payment
-        exclude = 'admin',
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
 
 
