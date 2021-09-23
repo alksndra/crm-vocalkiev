516eaa647c0e396d92d30c232366c9061064a5d0
AleksandraLyashchenco <juicyluck007@gmail.com>
2021-09-23 12:42:26 +0300

    removed LessonBase form

--- vocalkiev/apps/crm/apps/teacher/forms.py
+++ vocalkiev/apps/crm/apps/teacher/forms.py
@@ -18,9 +18,3 @@ class LessonAdminForm(forms.ModelForm):
             data['teacher'] = data['client_subscription'].teacher
         return data
 
-
-class LessonAdminBaseForm(forms.ModelForm):
-    class Meta:
-        model = Lesson
-        exclude = ('teacher',)
-
