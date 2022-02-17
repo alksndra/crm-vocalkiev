94f125bcf18b41b496fb152af6e3a751f9f86e6e
AleksandraLyashchenco <juicyluck007@gmail.com>
2022-02-17 08:47:34 +0200

    new method to show passed lessons in client subscription

--- vocalkiev/apps/crm/models.py
+++ vocalkiev/apps/crm/models.py
@@ -133,10 +133,11 @@ class ClientSubscription(Model):
     def can_create_lesson(self):
         return self.subscription.lessons_qty > self.lesson_set.count()
 
-    def can_archive(self):
-        passed_qty = self.lesson_set.filter(is_passed=True).count()
+    def get_passed_lessons_count(self):
+        return self.lesson_set.filter(is_passed=True).count()
 
-        return self.subscription.lessons_qty == passed_qty
+    def can_archive(self):
+        return self.subscription.lessons_qty == self.get_passed_lessons_count()
 
     def __str__(self):
         return f"{self.client}, {self.subscription}"
